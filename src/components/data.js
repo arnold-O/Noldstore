5933131e01682539a8956dece847b97d2ae05112
arnold-oghiator <arnoldoghiator@gmail.com>
2022-07-30 23:11:46 +0100

    adding dummy data in  data.js

---
+++ src/components/data.js
@@ -0,0 +1,50 @@
+export default  [
+
+    {id:1,
+    name:"Adidas SPORTS PERFORMANCE SOCKS 3S CSH CRW3P",
+price:"400",
+img:"https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/04/959568/1.jpg?3245",
+reviews:"",
+description:""
+},
+
+    {id:2,
+    name:"Arctic Hunter Halo Anti Theft Laptop Bag & Travel Backpack",
+price:"500",
+img:"https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/00/8476411/1.jpg?8823",
+reviews:"",
+description:""
+},
+
+    {id:3,
+    name:"Trendyol Collection Gray Men's Oversize Crew Neck Short Sleeve Printed T-Shirt",
+price:"300.00",
+img:"https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/71/9402721/1.jpg?4913",
+reviews:"",
+description:""
+},
+
+    {id:4,
+    name:"Sony 50W660 BRAVIA 50' Digital Smart FHD TV With Android OS",
+price:"800",
+img:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+reviews:"",
+description:""
+},
+
+    {id:5,
+    name:"Lenovo IdeaPad Core I3 4GB RAM 1TB ROM Freedos",
+price:"700",
+img:"https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/04/5722921/1.jpg?6735",
+reviews:"",
+description:""
+},
+
+    {id:6,
+    name:"Happy Baby Foldable Baby Crib With Net",
+price:"600",
+img:"https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/47/462473/1.jpg?3436",
+reviews:"",
+description:""
+},
+]
