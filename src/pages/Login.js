94d45d534aaa9b2aa4155686fbfe286fe561390f
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-02 23:51:41 +0100

    input box transitions

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,22 +4,26 @@ export default function Login() {
   return (
     <>
     <div className="main-container-wrapper">
+
        
 
         <div className='login-form-wrapper'>
 
   <div className='form-input-wrapper'>
+    <h1 style={{textAlign:"center"}}>Login</h1>
+    <form action="">
          
-  <div className='login-form'>
-        <input type="text"  id='email' className='input-form' placeholder='Enter your mail'/>
+  <div className='login-form-email'>
+        <input type="text"  id='email' className='input-form-email' placeholder='Enter your mail'/>
         <label htmlFor="email" className='form-label-email'></label>
     </div>
          
     <div className='login-form-pass'>
-        <input type="password"  id='password' className='input-form' placeholder='Enter your password'/>
+        <input type="password"  id='password' className='input-form-pass' placeholder='Enter your password'/>
         <label htmlFor="password" className='form-label-pass'></label>
     </div>
-    <button>Login</button>
+    <button className='login-button'>Login</button>
+  </form>
   </div>
 
     <div className='login-image-placeholder'>
