c275e0890a40920283786a8fcf826780771bea16
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-02 23:21:41 +0100

    button login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,11 +8,19 @@ export default function Login() {
 
         <div className='login-form-wrapper'>
 
-       
-    <div className='login-form'>
-        <input type="text"  id='email' className='input-form'/>
-        <label htmlFor="email" className='form-label'></label>
+  <div className='form-input-wrapper'>
+         
+  <div className='login-form'>
+        <input type="text"  id='email' className='input-form' placeholder='Enter your mail'/>
+        <label htmlFor="email" className='form-label-email'></label>
     </div>
+         
+    <div className='login-form-pass'>
+        <input type="password"  id='password' className='input-form' placeholder='Enter your password'/>
+        <label htmlFor="password" className='form-label-pass'></label>
+    </div>
+    <button>Login</button>
+  </div>
 
     <div className='login-image-placeholder'>
         <img src="login.svg" alt="" />
