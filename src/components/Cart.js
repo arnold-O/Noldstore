faba70d847897513dad769eddc514e94f323ec26
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-03 17:26:00 +0100

    cart ui

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,15 +2,18 @@ import React from "react";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
 import { TbCurrencyNaira } from "react-icons/tb";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import Footer from "./Footer";
 
 export default function Cart() {
   return (
+    <>
     <div className="main-container-wrapper">
       <div className="cart-section-wrapper">
         <div className="cart-item-section">
           <div>
             <p className="cart-item-header"><MdOutlineAddShoppingCart/><sup>3</sup></p>
           </div>
+          <div className="product-item-cart-wrapper">
           <div className="horizontal-cart" ></div>
           <div className="each-cart-item">
             <div className="cart-image-wrapper">
@@ -52,20 +55,75 @@ export default function Cart() {
              
               
             </div>
+            </div>
+            {/* new item */}
+            <div>
+          <div className="horizontal-cart" ></div>
+          <div className="each-cart-item">
+            <div className="cart-image-wrapper">
+              <img src="https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/64/629476/1.jpg?5803" alt="" />
+            </div>
+            <div className="each-item-details">
+              <div>
+
+              <p>Trendyol Collection Gray Men's Oversize Crew Neck Short Sleeve </p>
+              <div className="size-of-item">
+              <p>size:</p> 
+              <p>EU 41</p>
+              </div>
+              </div>
+
+              <div className="price-discount-wrapper">
+              <div className='price-wrapper-dicount-cart'>
+          <p><TbCurrencyNaira className='product-price-icon-discount'/></p><p className='product-current-price-cart'>5000</p>
+          </div>
+                <div className="oldprice-percent-wrapper">
+                  <p><TbCurrencyNaira className='product-price-icon-discount'/></p>
+                <p className="product-oldprice-cart">6500</p>
+                
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          <div className="counter-item-cart">
+
+              <p className="delete">Remove</p>
+              <div className="couter-item">
+              <AiOutlineMinus className="counter-minus"/>
+              <p>0</p>
+              <AiOutlinePlus className="counter-plus"/>
+              </div>
+
+        
+             
+              
+            </div>
+            </div>
+            
+            {/* new item */}
         </div>
         <div className="cart-item-sumary">
           <div className="cart-total-value">
-            <h3>Summary</h3>
+            <h3 >Summary</h3>
+            <div className="horizontal-cart" ></div>
 
-            <div>
+            <div className="subtotal-cart-amount">
               <p>Subtotal</p>
               <p>Amount</p>
             </div>
+
+           <div className="cart-button-wrapper">
+           <button className="checkout-cart-Btn">CheckOut</button>
+
+           </div>
 
 
           </div>
         </div>
       </div>
     </div>
+      <Footer/>
+      </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,74 +1,72 @@
-import React from 'react'
+import React from "react";
 import { GiBowTieRibbon, GiConverseShoe } from "react-icons/gi";
 import { MdRadio } from "react-icons/md";
 import { FaHeadphonesAlt } from "react-icons/fa";
 import { IoGameControllerOutline } from "react-icons/io5";
-import { Link } from 'react-router-dom';
-import Footer from './Footer';
-import Card from './Card';
-export default function Home({user}) {
+import { Link } from "react-router-dom";
+import Footer from "./Footer";
+import Card from "./Card";
+export default function Home({ user }) {
   return (
     <>
-    <div className='main-container-wrapper'>
-
-      <div className="headerWrapper">
-      <div className="headerWrapper-first">
-      <div className='category-section-left'>
-        <div className='category-filter-section'>
-         <GiBowTieRibbon className='header-icon'/>
-        <Link className="links"  to="fashion">Fashion</Link>
+      <div className="main-container-wrapper">
+        <div className="headerWrapper">
+          <div className="headerWrapper-first">
+            <div className="category-section-left">
+              <div className="category-filter-section">
+                <GiBowTieRibbon className="header-icon" />
+                <Link className="links" to="fashion">
+                  Fashion
+                </Link>
+              </div>
+              <div className="category-filter-section">
+                <GiConverseShoe className="header-icon" />
+                <Link className="links" to="shoes">
+                  Shoes
+                </Link>
+              </div>
+              <div className="category-filter-section">
+                <GiBowTieRibbon className="header-icon" />
+                <p style={{ color: "#000" }}>product</p>
+              </div>
+              <div className="category-filter-section">
+                <FaHeadphonesAlt className="header-icon" />
+                <Link className="links" to="headphones">
+                  Head Phones
+                </Link>
+              </div>
+              <div className="category-filter-section">
+                <MdRadio className="header-icon" />
+                <Link className="links" to="electronics">
+                  Electronics
+                </Link>
+              </div>
+              <div className="category-filter-section">
+                <GiConverseShoe className="header-icon" />
+                <Link className="links" to="gaming">
+                  Gaming
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="header-image-container">
+              <img
+                src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGVjb21tZXJjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="headerWrapper-first">
+            <p> hello, {user?.email}</p>
+          </div>
         </div>
-        <div className='category-filter-section'>
-       < GiConverseShoe className='header-icon'/>
-        <Link className="links"  to="shoes">Shoes</Link>
-        </div>
-        <div className='category-filter-section'>
-        <GiBowTieRibbon className='header-icon'/>
-        <p style={{color:"#000"}}>product</p>
-        </div>
-        <div className='category-filter-section'>
-      <FaHeadphonesAlt className='header-icon'/>
-        <Link className="links" to="headphones">Head Phones</Link>
-        </div>
-        <div className='category-filter-section'>
-        <MdRadio className='header-icon'/>
-        <Link className="links"  to="electronics">Electronics</Link>
-        </div>
-        <div className='category-filter-section'>
-        <GiConverseShoe className='header-icon'/>
-        <Link className="links"  to="gaming">Gaming</Link>
-        </div>
-      </div>
-      </div>
-      <div>
 
-      <div className='header-image-container'>
-        <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGVjb21tZXJjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-      </div>
-      
+        <h1 className="latest-product-title">Latest Product</h1>
 
-
-
+        <Card />
       </div>
-      <div className="headerWrapper-first">
-        <p> hello, {user?.email}</p>
-      
-        
-      </div>
-      </div>
-
-     
-      <h1 className='latest-product-title'>Latest Product</h1>
-    
-               
-       
-            <Card/>
-          
-          
-    
-     
-    </div>
-          <Footer/>
-          </>
-  )
+      <Footer />
+    </>
+  );
 }
