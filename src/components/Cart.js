35bcc8c8e66ed0e791248237951fd0dafc5c998a
arnold-oghiator <arnoldoghiator@gmail.com>
2022-08-05 23:34:55 +0100

    cart update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,125 +7,130 @@ import Footer from "./Footer";
 export default function Cart() {
   return (
     <>
-    <div className="main-container-wrapper">
-      <div className="cart-section-wrapper">
-        <div className="cart-item-section">
-          <div>
-            <p className="cart-item-header"><MdOutlineAddShoppingCart/><sup>3</sup></p>
-          </div>
-          <div className="product-item-cart-wrapper">
-          <div className="horizontal-cart" ></div>
-          <div className="each-cart-item">
-            <div className="cart-image-wrapper">
-              <img src="https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/64/629476/1.jpg?5803" alt="" />
+      <div className="main-container-wrapper">
+        <div className="cart-section-wrapper">
+          <div className="cart-item-section">
+            <div>
+              <p className="cart-item-header">
+                <MdOutlineAddShoppingCart />
+                <sup>3</sup>
+              </p>
             </div>
-            <div className="each-item-details">
-              <div>
+            <div className="product-item-cart-wrapper">
+              <div className="horizontal-cart"></div>
+              <div className="each-cart-item">
+                <div className="cart-image-wrapper">
+                  <img
+                    src="https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/64/629476/1.jpg?5803"
+                    alt=""
+                  />
+                </div>
+                <div className="each-item-details">
+                  <div>
+                    <p>
+                      Trendyol Collection Gray Men's Oversize Crew Neck Short
+                      Sleeve{" "}
+                    </p>
+                    <div className="size-of-item">
+                      <p>Size:</p>
+                      <p>EU 41</p>
+                    </div>
+                  </div>
 
-              <p>Trendyol Collection Gray Men's Oversize Crew Neck Short Sleeve </p>
-              <div className="size-of-item">
-              <p >Size:</p> 
-              <p>EU 41</p>
-              </div>
-              </div>
-
-              <div className="price-discount-wrapper">
-              <div className='price-wrapper-dicount-cart'>
-          <p><TbCurrencyNaira className='product-price-icon-discount'/></p><p className='product-current-price-cart'>5,000</p>
-          </div>
-                <div className="oldprice-percent-wrapper">
-                  <p><TbCurrencyNaira className='product-price-icon-discount'/></p>
-                <p className="product-oldprice-cart">6,500</p>
-                
+                  <div className="price-discount-wrapper">
+                    <div className="price-wrapper-dicount-cart">
+                      <p>
+                        <TbCurrencyNaira className="product-price-icon-discount" />
+                      </p>
+                      <p className="product-current-price-cart">5,000</p>
+                    </div>
+                    <div className="oldprice-percent-wrapper">
+                      <p>
+                        <TbCurrencyNaira className="product-price-icon-discount" />
+                      </p>
+                      <p className="product-oldprice-cart">6,500</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            
-          </div>
-          <div className="counter-item-cart">
-
-          <button  className="delete-eachItem-button">Remove</button>
-              <div className="couter-item">
-              <AiOutlineMinus className="counter-minus"/>
-              <p>0</p>
-              <AiOutlinePlus className="counter-plus"/>
+              <div className="counter-item-cart">
+                <button className="delete-eachItem-button">Remove</button>
+                <div className="couter-item">
+                  <AiOutlineMinus className="counter-minus" />
+                  <p>0</p>
+                  <AiOutlinePlus className="counter-plus" />
+                </div>
               </div>
-
-        
-             
-              
-            </div>
             </div>
             {/* new item */}
             <div>
-          <div className="horizontal-cart" ></div>
-          <div className="each-cart-item">
-            <div className="cart-image-wrapper">
-              <img src="https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/64/629476/1.jpg?5803" alt="" />
-            </div>
-            <div className="each-item-details">
-              <div>
+              <div className="horizontal-cart"></div>
+              <div className="each-cart-item">
+                <div className="cart-image-wrapper">
+                  <img
+                    src="https://ng.jumia.is/unsafe/fit-in/150x150/filters:fill(white)/product/64/629476/1.jpg?5803"
+                    alt=""
+                  />
+                </div>
+                <div className="each-item-details">
+                  <div>
+                    <p>
+                      Trendyol Collection Gray Men's Oversize Crew Neck Short
+                      Sleeve{" "}
+                    </p>
+                    <div className="size-of-item">
+                      <p>Size:</p>
+                      <p>EU 41</p>
+                    </div>
+                  </div>
 
-              <p>Trendyol Collection Gray Men's Oversize Crew Neck Short Sleeve </p>
-              <div className="size-of-item">
-              <p>Size:</p> 
-              <p>EU 41</p>
+                  <div className="price-discount-wrapper">
+                    <div className="price-wrapper-dicount-cart">
+                      <p>
+                        <TbCurrencyNaira className="product-price-icon-discount" />
+                      </p>
+                      <p className="product-current-price-cart">5,000</p>
+                    </div>
+                    <div className="oldprice-percent-wrapper">
+                      <p>
+                        <TbCurrencyNaira className="product-price-icon-discount" />
+                      </p>
+                      <p className="product-oldprice-cart">6,500</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              </div>
+              <div className="counter-item-cart">
+                <button className="delete-eachItem-button">Remove</button>
 
-              <div className="price-discount-wrapper">
-              <div className='price-wrapper-dicount-cart'>
-          <p><TbCurrencyNaira className='product-price-icon-discount'/></p><p className='product-current-price-cart'>5,000</p>
-          </div>
-                <div className="oldprice-percent-wrapper">
-                  <p><TbCurrencyNaira className='product-price-icon-discount'/></p>
-                <p className="product-oldprice-cart">6,500</p>
-                
+                <div className="couter-item">
+                  <AiOutlineMinus className="counter-minus" />
+                  <p>1</p>
+                  <AiOutlinePlus className="counter-plus" />
                 </div>
               </div>
             </div>
-            
-          </div>
-          <div className="counter-item-cart">
 
-           
-                <button  className="delete-eachItem-button">Remove</button>
-             
-              <div className="couter-item">
-              <AiOutlineMinus className="counter-minus"/>
-              <p>1</p>
-              <AiOutlinePlus className="counter-plus"/>
+            {/* new item */}
+          </div>
+          <div className="cart-item-sumary">
+            <div className="cart-total-value">
+              <h3>Summary</h3>
+              <div className="horizontal-cart"></div>
+
+              <div className="subtotal-cart-amount">
+                <p>Subtotal</p>
+                <p>Amount</p>
               </div>
 
-        
-             
-              
+              <div className="cart-button-wrapper">
+                <button className="checkout-cart-Btn">CheckOut</button>
+              </div>
             </div>
-            </div>
-            
-            {/* new item */}
-        </div>
-        <div className="cart-item-sumary">
-          <div className="cart-total-value">
-            <h3 >Summary</h3>
-            <div className="horizontal-cart" ></div>
-
-            <div className="subtotal-cart-amount">
-              <p>Subtotal</p>
-              <p>Amount</p>
-            </div>
-
-           <div className="cart-button-wrapper">
-           <button className="checkout-cart-Btn">CheckOut</button>
-
-           </div>
-
-
           </div>
         </div>
       </div>
-    </div>
-      <Footer/>
-      </>
+      <Footer />
+    </>
   );
 }
--- src/context.js
+++ src/context.js
@@ -1,48 +1,32 @@
-import React, { useContext, useEffect, useReducer, useState } from "react"
+import React, { useContext, useEffect, useReducer, useState } from "react";
 import data from "./components/data";
-
 
 import reducer from "./reducer";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+  total: 0,
+  amount: 0,
+  product: data,
+};
 
-    const initialState = {
-        loading:false,
-        cart:[],
-        total:0,
-        amount:0,
-        product:data
-    }
-    
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  const addToCart = () => {
+    dispatch({ type: "ADD_TO_CART" });
+  };
 
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
 
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
-const AppProvider = ({children})=>{
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-
-
-    const addToCart = () =>{
-      dispatch({type:"ADD_TO_CART"})
-    }
-    
-
-
-
-   return <AppContext.Provider
-    value={{ ...state
-      
-    }}
-  >
-    {children}
-  </AppContext.Provider>
-
-}
-
-export const useGlobalContext = ()=>{
-    return useContext(AppContext)
-}
-
-export {AppProvider, AppContext }
+export { AppProvider, AppContext };
